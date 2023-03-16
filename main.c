d6996812d7a99c87de9737077bc3c9f2525521c6
おもちくん <sei.t11@icloud.com>
2023-03-16 09:40:19 +0900

    del main com

--- main.c
+++ main.c
@@ -18,32 +18,5 @@ void test_get_file_name(){
 int main(int argc, char const *argv[]){
     test_get_file_name();
 
-    // char *s = "ABCDEFGABCDEFG";
-    // int c = 'C';
-    // char *ret;
-
-    // ret = strrchr( s, c );
-    // if( ret != NULL ){
-    //     printf( "%cを%ld番目に発見しました\n", c, ret - s );
-    //     printf("%s\n",ret); // CDEFG
-    //     ret++;
-    //     printf("%s\n",ret); // DEFG
-    // }
-    // else{
-    //     printf( "%cは発見出来ません¥n", c );
-    // }
-
-    // char* test1 = "/users/aaa/doc/aaa";
-    // char slash = '/';
-    // char* file_name;
-
-    // file_name = strrchr(test1,slash);
-    // printf("%s\n",file_name);
-
-    // // この場合に /f を出力させたい
-    // char* test2 = "/aaa/ssss/f/";
-    // file_name = strrchr(test2,slash);
-    // printf("%s\n",file_name);
-
     return 0;
 }
